e2ff4a596d8edea7d7be4fe3526c50728af2f0b4
Oscar Cruz <cruz.arellano.oscar@gmail.com>
2014-03-06 22:29:01 -0600

    Primer commit, se sube todo

---
+++ index.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Emanuel Martinez Avaluos</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/avaluos.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-1.10.2.min.js"></script>
+
+</head>
+<body>
+	<div id="page-container" rol="page-container">
+		<header id="main-header">
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="navbar-container">
+					<h1 class="navbar-brand sitetitle">Ávaluos</h1>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="/">Inicio</a></li>
+					<li><a href="nosotros.php">¿Quienes somos?</a></li>
+					<li><a href="servicios.php">Servicios</a></li>
+					<li><a href="asesoria.php">Asesoria profesional</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section id="main-container" class="container">
+			<div class="img-slide">
+				<img src="img/MAIN.jpg" alt="Avaluos">
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<img class="img-col-md-4" src="img/signo_pesos.png" alt="Avaluos">
+					<h2>Ávaluos</h2>
+					<p class="normaltext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<br><br>
+					Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="col-md-4">
+					<img class="img-col-md-4" src="img/consultoria.png" alt="Consultoria">
+					<h2>Consultoria</h2>
+					<p class="normaltext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					<br><br>
+					Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="col-md-4">
+					<img class="img-col-md-4" src="img/contacto.png" alt="Contacto">
+					<h2>Contacto</h2>
+					<p class="normaltext">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<button type="button" class="btn btn-custom-blue">Email</button>
+				</div>
+			</div>
+			<div class="row">
+				<h2>Últimos proyectos</h2>
+				<div class="col-md-4">
+					<img src="http://www.metrocuadrado.com/decoracion/sites/metrocuadrado.com/files/imagecache/DimensionesImagenes/avaluocomercialyelcatastral.jpg.crop_display.jpg" alt="Avaluo" class="img_proyecto">
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.
+					</p>
+				</div>
+				<div class="col-md-4">
+					<img src="http://www.metrocuadrado.com/decoracion/sites/metrocuadrado.com/files/imagecache/DimensionesImagenes/avaluocomercialyelcatastral.jpg.crop_display.jpg" alt="Avaluo" class="img_proyecto">
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.
+					</p>
+				</div>
+			</div>
+		</section>
+		<footer id="main-footer" class="">
+			<div class="row">
+				<div class="col-md-4">
+					<span class="sitetitle">Ávaluos</span>
+					<p class="copy">2014 Todos los derechos reservados</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Servicios</h2>
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Contacto</h2>
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ nosotros.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Emanuel Martinez Avaluos</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/avaluos.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery-1.10.2.min.js"></script>
+
+</head>
+<body>
+	<div id="page-container" rol="page-container">
+		<header id="main-header">
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="navbar-container">
+					<h1 class="navbar-brand sitetitle">Ávaluos</h1>
+				</div>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/">Inicio</a></li>
+					<li class="active"><a href="nosotros.php">¿Quienes somos?</a></li>
+					<li><a href="servicios.php">Servicios</a></li>
+					<li><a href="asesoria.php">Asesoria profesional</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section id="main-container" class="container">
+			<h2 class="pagetitle">¿Quienes somos?</h2>
+			<p class="normaltext">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p class="normaltext">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<h3 class="sibtitles">Misión</h3>
+			<p class="normaltext">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<h3 class="sibtitles">Visión</h3>
+			<p class="normaltext">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</section>
+		<footer id="main-footer" class="">
+			<div class="row">
+				<div class="col-md-4">
+					<span class="sitetitle">Ávaluos</span>
+					<p class="copy">2014 Todos los derechos reservados</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Servicios</h2>
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</div>
+				<div class="col-md-4">
+					<h2>Contacto</h2>
+					<p class="normaltext">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+				</div>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
