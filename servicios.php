8f772c7e151686332dfa170480fc05d86ef3646e
Oscar Cruz <cruz.arellano.oscar@gmail.com>
2014-03-23 19:38:03 -0600

    Cambio en la pagina servicios

--- servicios.php
+++ servicios.php
@@ -26,41 +26,27 @@
 			</nav>
 		</header>
 		<section id="main-container" class="container">
-			<h2 class="pagetitle">¿Quienes somos?</h2>
-			<p class="normaltext">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-			<p class="normaltext">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-			<h3 class="sibtitles">Misión</h3>
-			<p class="normaltext">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-			<h3 class="sibtitles">Visión</h3>
-			<p class="normaltext">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<h2 class="pagetitle">¿Que servicios ofrecemos?</h2>
+				<p class="normaltext">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>	
+			</div>
+			<div class="col-xs-12 col-sm-11 col-md-9 col-lg-9 service-box">
+				<h3 class="sibtitles">Nuestros Servicios</h3>
+				<ul class="service-list">
+					<li>Avaluos Inmuebles</li>
+					<li>Avaluos Inmuebles</li>
+					<li>Avaluos Inmuebles</li>
+					<li>Avaluos Inmuebles</li>
+					<li>Avaluos Inmuebles</li>
+				</ul>
+			</div>
 		</section>
 		<footer id="main-footer">
 			<div class="row">
